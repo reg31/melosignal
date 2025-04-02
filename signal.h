69ee767f3207d5d2b14d0149a5f62b59fca9c97a
reg31 <37986256+reg31@users.noreply.github.com>
2025-04-02 17:00:20 +0100

    call invokemethod on QObject

--- signal.h
+++ signal.h
@@ -4,9 +4,11 @@
 #include <vector>
 #include <QThread>
 #include <QPointer>
+#include <QObject>
 #include <functional>
 #include <QMetaObject>
 #include <QReadWriteLock>
+#include <type_traits>
 
 namespace melo {
 
@@ -19,6 +21,7 @@ private:
     struct Slot {
         Callback callback;
         QPointer<QThread> thread;
+        QPointer<QObject> qobject_instance;
     };
 
     std::vector<Slot> slots{};
@@ -27,32 +30,41 @@ private:
     inline void insert(Callback&& callee)
     {
         QWriteLocker locker(&lock);
-        slots.emplace_back(Slot{std::move(callee), QThread::currentThread()});
+        slots.emplace_back(Slot{std::move(callee), QThread::currentThread(), nullptr});
     }
 
 public:
     ~signal() = default;
     signal() noexcept = default;
 
-    // ✅ Support function pointers and lamdas
+    // Support function pointers and lamdas
     template <typename Function>
     void connect(Function&& callee)
     {
         insert(std::move(callee));
     }
 
-    // ✅ Support member functions with different reference types
+    // Support member functions with different reference types
     template <typename ClassType, typename Function>
     void connect(ClassType* instance, Function&& member_function)
     {
-        insert (
-            [instance, member_function](Args&&... args) {
+        if constexpr (std::is_base_of_v<QObject, ClassType>) {
+            QWriteLocker locker(&lock);
+            slots.emplace_back(Slot{
+                [instance, member_function](Args&&... args) {
+                    std::invoke(member_function, instance, std::forward<Args>(args)...);
+                },
+                QThread::currentThread(),
+                instance
+            });
+        } else {
+            insert([instance, member_function](Args&&... args) {
                 std::invoke(member_function, instance, std::forward<Args>(args)...);
-            }
-        );
+            });
+        }
     }
 
-    // ✅ Support connecting one signal to another
+    // Support connecting one signal to another
     template <typename OtherSignal>
     void connect(OtherSignal &other)
     {
@@ -73,10 +85,20 @@ public:
         {
             if(slot.callback != nullptr)
             {
-                if (slot.thread->isCurrentThread()) {
+				if (slot.qobject_instance) {
+                    QMetaObject::invokeMethod(
+                        slot.qobject_instance, 
+                        [cb = slot.callback, args...] { cb(args...); }, 
+                        Qt::AutoConnection
+                    );
+				} else if (slot.thread->isCurrentThread()) {
                     slot.callback(args...);
                 } else {
-                    QMetaObject::invokeMethod(slot.thread, [&cb = slot.callback, args...] {cb(args...);}, Qt::QueuedConnection);
+                    QMetaObject::invokeMethod(
+                        slot.thread, 
+                        [cb = slot.callback, args...] { cb(args...); }, 
+                        Qt::QueuedConnection
+                    );
                 }
             }
         }
