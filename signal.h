9871819066d85694223c264494140d8d9426f9d6
reg31 <37986256+reg31@users.noreply.github.com>
2025-04-02 22:10:09 +0100

    Improve safety using concepts and simplify code

--- signal.h
+++ signal.h
@@ -20,17 +20,16 @@ private:
 
     struct Slot {
         Callback callback;
-        QPointer<QThread> thread;
-        QPointer<QObject> qobject_instance;
+        QPointer<QObject> qobject;
     };
 
     std::vector<Slot> slots{};
     QReadWriteLock lock;
 
-    inline void insert(Callback&& callee)
+    inline void insert(Callback&& callee, QPointer<QObject> obj = nullptr)
     {
         QWriteLocker locker(&lock);
-        slots.emplace_back(Slot{std::move(callee), QThread::currentThread(), nullptr});
+        slots.emplace_back(Slot{std::move(callee), obj? obj : QThread::currentThread()});
     }
 
 public:
@@ -39,6 +38,7 @@ public:
 
     // Support function pointers and lamdas
     template <typename Function>
+	requires std::invocable<Function, Args...>
     void connect(Function&& callee)
     {
         insert(std::move(callee));
@@ -46,26 +46,23 @@ public:
 
     // Support member functions with different reference types
     template <typename ClassType, typename Function>
+	requires std::invocable<Function, ClassType*, Args...>
     void connect(ClassType* instance, Function&& member_function)
     {
+		QPointer<QObject> obj = nullptr;
+		
         if constexpr (std::is_base_of_v<QObject, ClassType>) {
-            QWriteLocker locker(&lock);
-            slots.emplace_back(Slot{
-                [instance, member_function](Args&&... args) {
-                    std::invoke(member_function, instance, std::forward<Args>(args)...);
-                },
-                QThread::currentThread(),
-                instance
-            });
-        } else {
-            insert([instance, member_function](Args&&... args) {
-                std::invoke(member_function, instance, std::forward<Args>(args)...);
-            });
-        }
+            obj = instance;
+		}
+		
+		insert([instance, member_function](Args&&... args) {
+			std::invoke(member_function, instance, std::forward<Args>(args)...);
+		}, obj);
     }
 
     // Support connecting one signal to another
     template <typename OtherSignal>
+	requires std::same_as<OtherSignal, signal<Args...>>
     void connect(OtherSignal &other)
     {
         insert([&other](Args&&... args) { other.emit(std::forward<Args>(args)...); });
@@ -83,23 +80,13 @@ public:
 
         for (const Slot &slot : slots)
         {
-            if(slot.callback != nullptr)
+            if(slot.callback && slot.qobject)
             {
-				if (slot.qobject_instance) {
-                    QMetaObject::invokeMethod(
-                        slot.qobject_instance, 
-                        [cb = slot.callback, args...] { cb(args...); }, 
-                        Qt::AutoConnection
-                    );
-				} else if (slot.thread->isCurrentThread()) {
-                    slot.callback(args...);
-                } else {
-                    QMetaObject::invokeMethod(
-                        slot.thread, 
-                        [cb = slot.callback, args...] { cb(args...); }, 
-                        Qt::QueuedConnection
-                    );
-                }
+				QMetaObject::invokeMethod(
+					slot.qobject, 
+					[cb = slot.callback, ...args = std::move(args)] { cb(args...); },
+					Qt::AutoConnection
+				);			
             }
         }
     }
